e42f793edea2e894c7b57a14033c5a64607336ef
Antonio Rodríguez Padilla <Antoniorp1980@gmail.com>
2016-10-06 14:21:00 +0200

    añadido ejercicios

---
+++ Capitulo4/Variables01.java
@@ -0,0 +1,31 @@
+/**
+ *
+ * Programa para decir qué toca a primera hora cada día de la semana introducido.
+ *
+ * @author Antonio R.P.
+ */
+
+public class  Variables01 {
+  public static void main(String[] args) {
+
+    System.out.print("Por favor, introduzca día de la semana: ");
+    String dia = (System.console().readLine()); 
+    dia.toLowerCase();
+    
+    if (dia.equals ("lunes")) {
+      System.out.print("a primera hora toca S.I");
+    } else if (dia.equals ("martes")) {
+      System.out.print("a primera hora toca Prog");
+    } else if (dia.equals ("miércoles")) {
+      System.out.print("a primera hora toca Prog");
+    } else if (dia.equals ("jueves")) {
+      System.out.print("a primera hora toca B.D.");
+    } else if (dia.equals ("viernes")) {
+      System.out.print("a primera hora toca L.M.");
+    } else {
+      System.out.print("eso no es un día de la semana");
+    }
+      
+      
+  }
+}
